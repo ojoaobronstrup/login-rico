7c2098ab33165e7142631464dc943b30d4090be0
João Brönstrup <joaog.gob@gmail.com>
2023-09-22 06:27:06 -0300

    tornando dinâmica a senha

--- src/componentes/login/index.js
+++ src/componentes/login/index.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react'
 import Senha from './senha'
 
-export default function login () {
+export default function Login() {
     const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+     const [pos, setPos] = useState(Math.floor(Math.random() * 10))
+    const [setPosValue, setSetPosValue] = useState(Math.floor(Math.random() * 10))
+
+    // Função para gerar um número aleatório diferente de pos
+    const posDiferente = () => {
+        let newNumber;
+        do {
+            newNumber = Math.floor(Math.random() * 10);
+        } while (newNumber === pos)
+        return newNumber;
+    }
+
+    useEffect(() => {
+        setPos(posDiferente())
+        setSetPosValue(posDiferente())
+    }, [])
 
     return (
         <div className="h-[50vh] bg-yellow-200 mx-3">
-            <Senha num1={`${numeros[0]} ou ${numeros[1]}`}/>
+            <Senha num1={`${numeros[pos]} ou ${numeros[setPosValue]}`}/>
         </div>
     )
 }
