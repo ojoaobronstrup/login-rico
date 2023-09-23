ca29af1d67bad71884d36838d0557daf189023b9
João Brönstrup <joaog.gob@gmail.com>
2023-09-23 10:16:24 -0300

    aprimorando sistema de login

--- src/componentes/login/index.js
+++ src/componentes/login/index.js
@@ -7,15 +7,16 @@ export default function Login() {
     useEffect(() => {
         let newPos = []
         while(newPos.length < 10){
-            let r = Math.floor(Math.random() * 10)
-            if(newPos.indexOf(r) === -1) newPos.push(r)
-            console.log(newPos)
+            let r = Math.floor(Math.random() * 10) //Cria um nº aleatório e armazena em "r"
+            if(newPos.indexOf(r) === -1) {
+                newPos.push(r)
+            }
         }
         setPos(newPos)
     }, [])
 
     return (
-        <div className="h-[50vh] bg-yellow-200 mx-3">
+        <div className="h-[50vh] mx-3">
             <Senha num1={`${pos[0]} ou ${pos[1]}`} num2={`${pos[2]} ou ${pos[3]}`} num3={`${pos[4]} ou ${pos[5]}`} num4={`${pos[6]} ou ${pos[7]}`} num5={`${pos[8]} ou ${pos[9]}`}/>
         </div>
     )
